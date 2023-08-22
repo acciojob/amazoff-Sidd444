3bee800b53622c711910ebfc774e123a3b5972c0
Sidd444 <siddharthabharaliassam@gmail.com>
2023-08-22 21:07:14 +0530

    Assignment done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -6,7 +6,11 @@ public class Order {
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
-
+         this.id=id;
+         int dt=Integer.parseInt(deliveryTime);
+         int h=dt/60,m=dt%60;
+         String DT=String.format("%02d:%02d",h,m);
+         this.deliveryTime=Integer.parseInt(DT);
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
     }
